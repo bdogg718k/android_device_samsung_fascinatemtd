a7c8f6825011bf7deee96ebb671d86140ff0bfea
C457 <android.c357@gmail.com>
2013-11-24 00:17:30 -0600

    Modify for nexus modem

--- fascinatemtd.mk
+++ fascinatemtd.mk
@@ -43,6 +43,5 @@ $(call inherit-product, device/samsung/fascinatemtd/device.mk)
 PRODUCT_PROPERTY_OVERRIDES += \
        ro.cdma.home.operator.numeric=310004 \
        ro.cdma.home.operator.alpha=Verizon \
-       net.cdma.pppd.user=user[SPACE]VerizonWireless
 
 TARGET_OTA_ASSERT_DEVICE := aries,fascinate,fascinatemtd,SCH-I500
