b9ad83eeb946c5ec5da52c6aa6b6f5c0c3ee88e9
C457 <android.c357@gmail.com>
2013-12-04 06:18:13 -0600

    Revert "Update ril"
    This reverts commit e1e487e2d950eb4b2b164cb168faa713781ab689.

--- device.mk
+++ device.mk
@@ -161,7 +161,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
        net.interfaces.defaultroute=cdma \
        mobiledata.interfaces=ppp0 \
        ro.ril.samsung_cdma=true \
-       ro.telephony.ril_class=SamsungExynos3CDMARIL \
+       ro.telephony.ril_class=SamsungExynosCDMA3RIL \
        ro.telephony.ril.v3=datacall \
        ro.bq.gpu_to_cpu_unsupported=1
 
